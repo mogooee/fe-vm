b7a98e5d0d0eb454d812655a43835bb789a41df7
연 <mogoo22@naver.com>
2022-05-11 15:25:15 +0900

    refactor: VendController Component의 하위컴포넌트 분리

--- src/components/VendController.jsx
+++ src/components/VendController.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { HistoryBox } from "components";
+import { InsertCoin, ChangeOutlet, HistoryBox } from "components";
 
 const StyledVendController = styled.div`
   width: 300px;
@@ -16,37 +16,11 @@ const StyledVendController = styled.div`
   }
 `;
 
-const InsertCoin = styled.input`
-  width: 100%;
-  height: 70px;
-  background-color: #f2f2f2;
-  border: none;
-  border-radius: 10px;
-  margin: 0px 10px 10px 10px;
-  text-align: center;
-  font-size: 1.5em;
-
-  &:focus {
-    outline: none;
-  }
-`;
-
-const ChangeOutlet = styled.div`
-  height: 70px;
-  background-color: #000;
-  margin-left: 10px;
-  color: #fff;
-
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
 function VendController() {
   return (
     <StyledVendController>
-      <InsertCoin type="number" min="0" placeholder="INSERT COIN" />
-      <ChangeOutlet>Change</ChangeOutlet>
+      <InsertCoin />
+      <ChangeOutlet />
       <HistoryBox />
     </StyledVendController>
   );

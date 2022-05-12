2519ac80d0e6b2edd3d36fbbb5db1be623bca725
연 <mogoo22@naver.com>
2022-05-12 15:30:30 +0900

    refactor: CoinContainer, VendProductContainer Component 분리

--- src/components/vending-machine/vend-product/VendProduct.jsx
+++ src/components/vending-machine/vend-product/VendProduct.jsx
@@ -1,22 +1,5 @@
-import { product } from "data";
 import styled from "styled-components";
 import { Button } from "components";
-
-const VendProductContainer = styled.ul`
-  display: grid;
-  grid-template-columns: 25% 25% 25% 25%;
-
-  li {
-    background-color: #f2f2f2;
-    border: 1px solid;
-    padding: 10px;
-
-    button {
-      border: 1px solid;
-      border-color: #2fc4b2;
-    }
-  }
-`;
 
 const PriceLabel = styled.span`
   display: grid;
@@ -24,19 +7,15 @@ const PriceLabel = styled.span`
   margin-top: 20px;
 `;
 
-function Vendproduct() {
+function VendProduct({ name, price, stocked }) {
   return (
-    <VendProductContainer>
-      {product.map(({ id, name, price, stocked }) => (
-        <li key={id}>
-          <Button color={stocked ? "green" : "white"} size="small">
-            <strong>{name}</strong>
-          </Button>
-          <PriceLabel>{price + "원"}</PriceLabel>
-        </li>
-      ))}
-    </VendProductContainer>
+    <>
+      <Button color={stocked ? "green" : "white"} size="small">
+        <strong>{name}</strong>
+      </Button>
+      <PriceLabel>{price + "원"}</PriceLabel>
+    </>
   );
 }
 
-export { Vendproduct };
+export { VendProduct };
--- src/components/wallet/Coin.jsx
+++ src/components/wallet/Coin.jsx
@@ -1,38 +1,13 @@
-import styled from "styled-components";
 import { Button } from "components";
 
-const CoinContainer = styled.ul`
-  li {
-    display: grid;
-    justify-items: center;
-    align-items: center;
-    grid-template-columns: 50% 50%;
-    margin: 10px;
-
-    span {
-      width: 100%;
-      background-color: #fff7bc;
-      display: grid;
-      place-items: center;
-      height: 50px;
-      color: #000;
-      border-radius: 10px;
-    }
-  }
-`;
-
-function Coin({ coin }) {
+function Coin({ unit, count }) {
   return (
-    <CoinContainer>
-      {coin.map(({ id, unit, count }) => (
-        <li key={id}>
-          <Button color="yellow" size="medium">
-            <strong>{unit}</strong>
-          </Button>
-          <span>{count}</span>
-        </li>
-      ))}
-    </CoinContainer>
+    <>
+      <Button color="yellow" size="medium">
+        <strong>{unit}</strong>
+      </Button>
+      <span>{count}</span>
+    </>
   );
 }
 

d6ebc4599e2d98e3fc95aac54631e6f4dd68b01f
연 <mogoo22@naver.com>
2022-05-22 15:07:35 +0900

    refactor: for문 for of문으로 변경

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,17 +1,17 @@
 const near = (arr, target) => {
+  if (target > arr[arr.length - 1]) return arr[arr.length - 1];
+
   let near = 0;
   let abs = 0;
   let min = 10000;
 
-  if (target > arr[arr.length - 1]) return arr[arr.length - 1];
-
-  for (let i = 0; i < arr.length; i++) {
-    abs = arr[i] - target;
+  for (const element of arr) {
+    abs = element - target;
     abs = abs < 0 ? -abs : abs;
 
     if (abs < min) {
       min = abs;
-      near = arr[i];
+      near = element;
     }
   }
 
